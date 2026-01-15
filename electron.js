045fcd466106644333eb4330e80202159e223112
yaaannts <76771816+yaaannts@users.noreply.github.com>
2026-01-15 17:25:27 +0500

    Set up Electron app with main window configuration

--- electron.js
+++ electron.js
@@ -1,1 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 900,
+        webPreferences: {
+            nodeIntegration: false,
+            webSecurity: true
+        },
+        icon: path.join(__dirname, 'icon.png') // Ensure you have icon.png in root
+    });
+
+    // Load the index.html file
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
